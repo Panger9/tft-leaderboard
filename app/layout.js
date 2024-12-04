3d34b62807b18792c1dcd326fa1bf5fe5b007fe7
Panger9 <fabio.pellegrino1011@gmail.com>
2024-12-04 09:53:42 +0100

    Tag manger versuch 2

--- app/layout.js
+++ app/layout.js
@@ -23,28 +23,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script>
-          {(function (w, d, s, l, i) {
-            w[l] = w[l] || []
-            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != "dataLayer" ? "&l=" + l : ""
-            j.async = true
-            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
-            f.parentNode.insertBefore(j, f)
-          })(window, document, "script", "dataLayer", "GTM-KFQ95DGQ")}
-        </Script>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-DQK6T956S1"
-          strategy="afterInteractive"
-        />
         <Script id="google-analytics" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-DQK6T956S1');`}
+          {`
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KFQ95DGQ');</script>
+          `}
         </Script>
       </head>
       <body
